aa4c43853efd38c4f470db13ef8c93cf53d8c19b
Enric Enrich <enrichenr@gmail.com>
2016-11-25 17:07:23 +0100

    Minor fixes on the Swift 3 migration

--- Example/EECellSwipeGestureRecognizerDemo/MainViewController.swift
+++ Example/EECellSwipeGestureRecognizerDemo/MainViewController.swift
@@ -57,23 +57,24 @@ class MainViewController: UITableViewController {
     
     fileprivate lazy var rightPushSwipeCell: UITableViewCell = {
         let cell: UITableViewCell = UITableViewCell()
+        cell.contentView.backgroundColor = UIColor.white
         cell.textLabel?.text = "Right Push"
         
         let slideGestureRecognizer: EECellSwipeGestureRecognizer = EECellSwipeGestureRecognizer()
         
         let rightPushAction: EECellSwipeAction = EECellSwipeAction(fraction: 0.25)
         rightPushAction.icon = UIImage(named: "circle")!
-        rightPushAction.activeBackgroundColor = UIColor.greenColor()
-        rightPushAction.behavior = .Push
+        rightPushAction.activeBackgroundColor = UIColor.green
+        rightPushAction.behavior = .push
         rightPushAction.didTrigger = { (tableView, indexPath) in
             print("Right Push")
             
-            let alert = UIAlertController(title: "Swiped", message: "Right Push", preferredStyle: UIAlertControllerStyle.Alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            let alert = UIAlertController(title: "Swiped", message: "Right Push", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) -> Void in
                 slideGestureRecognizer.swipeToOrigin(true, completion: nil)
             }))
             
-            self.presentViewController(alert, animated: true, completion: nil)
+            self.present(alert, animated: true, completion: nil)
         }
         
         slideGestureRecognizer.addActions([rightPushAction])
@@ -84,14 +85,15 @@ class MainViewController: UITableViewController {
     
     fileprivate lazy var leftPullSwipeCell: UITableViewCell = {
         let cell: UITableViewCell = UITableViewCell()
+        cell.contentView.backgroundColor = UIColor.white
         cell.textLabel?.text = "Left Pull"
         
         let slideGestureRecognizer: EECellSwipeGestureRecognizer = EECellSwipeGestureRecognizer()
         
         let leftPullAction: EECellSwipeAction = EECellSwipeAction(fraction: -0.25)
         leftPullAction.icon = UIImage(named: "circle")!
-        leftPullAction.activeBackgroundColor = UIColor.blueColor()
-        leftPullAction.behavior = .Pull
+        leftPullAction.activeBackgroundColor = UIColor.blue
+        leftPullAction.behavior = .pull
         leftPullAction.didTrigger = { (tableView, indexPath) in
             print("Left Pull")
         }
@@ -104,29 +106,30 @@ class MainViewController: UITableViewController {
     
     fileprivate lazy var rightAndLeftSwipeCell: UITableViewCell = {
         let cell: UITableViewCell = UITableViewCell()
+        cell.contentView.backgroundColor = UIColor.white
         cell.textLabel?.text = "Right Push & Left Pull"
         
         let slideGestureRecognizer: EECellSwipeGestureRecognizer = EECellSwipeGestureRecognizer()
         
         let rightPushAction: EECellSwipeAction = EECellSwipeAction(fraction: 0.25)
         rightPushAction.icon = UIImage(named: "circle")!
-        rightPushAction.activeBackgroundColor = UIColor.greenColor()
-        rightPushAction.behavior = .Push
+        rightPushAction.activeBackgroundColor = UIColor.green
+        rightPushAction.behavior = .push
         rightPushAction.didTrigger = { (tableView, indexPath) in
             print("Right Push")
             
-            let alert = UIAlertController(title: "Swiped", message: "Right Push", preferredStyle: UIAlertControllerStyle.Alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            let alert = UIAlertController(title: "Swiped", message: "Right Push", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) -> Void in
                 slideGestureRecognizer.swipeToOrigin(true, completion: nil)
             }))
             
-            self.presentViewController(alert, animated: true, completion: nil)
+            self.present(alert, animated: true, completion: nil)
         }
         
         let leftPullAction: EECellSwipeAction = EECellSwipeAction(fraction: -0.25)
         leftPullAction.icon = UIImage(named: "circle")!
-        leftPullAction.activeBackgroundColor = UIColor.blueColor()
-        leftPullAction.behavior = .Pull
+        leftPullAction.activeBackgroundColor = UIColor.blue
+        leftPullAction.behavior = .pull
         leftPullAction.didTrigger = { (tableView, indexPath) in
             print("Left Pull")
         }
