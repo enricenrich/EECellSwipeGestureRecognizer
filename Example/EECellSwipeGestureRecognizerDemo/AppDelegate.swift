921c6f8468587fcd927621a8855fd192cc942e69
Enric Enrich <enric@enric.co>
2018-10-30 16:44:59 +0100

    Minor fixes to the demo

--- Example/EECellSwipeGestureRecognizerDemo/AppDelegate.swift
+++ Example/EECellSwipeGestureRecognizerDemo/AppDelegate.swift
@@ -13,14 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        
+
         let navigationController: UINavigationController = UINavigationController(rootViewController: MainViewController())
-        
+
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
-        
+
         return true
     }
 
